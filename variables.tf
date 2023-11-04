b928e6767e07e20c7a4ee4122cf7ed4493cd8561
FonsahPageo <ashprincepageo@gmail.com>
2023-11-04 09:51:46 +0100

    modified git repo in jenkinsfile, vpc_id, sg_id, keyname, instance_type, region in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,35 +1,29 @@
 variable "region" {
-  default = "us-east-1"
+  default = "us-east-2"
 }
-
 variable "tag" {
-  default = "Demo-Instance"
+  default = "terraform-pipeline"
 }
-
 variable "ami_name_value" {
   default = "amzn-ami-vpc-nat*"
 }
-
 variable "ami_virtualization_type" {
   default = "hvm"
 }
-
 variable "instance_type" {
   default = "t2.micro"
 }
-
 variable "iscreate" {
   default = "true"
 }
 
 variable "sg_id" {
-  default = "sg-0d61315e929c9b42c"
+  default = "sg-08484f5a049db1fa4"
 }
-
 
 variable "keyname" {
-  default = "EAST-KEY"
+  default = "Fonsah_key_pair_Ohio"
 }
 variable "vpc_id" {
-  default = "vpc-070c3c4ddf310be5d"
+  default = "vpc-02851b8007ea7a315"
 }
